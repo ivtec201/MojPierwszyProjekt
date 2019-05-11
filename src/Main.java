04d1bd406036d5d9e322404b482ce216b148dd1a
ivtec201 <ivtec201@gmail.com>
2019-05-11 14:03:47 +0200

    Petla

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {            //commit zapisuje, a push wypycha na serwer; -m znaczy, że będzie wiadomość
-        System.out.println("Witaj, Swiecie!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
