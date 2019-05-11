168a6a4fb8dfe5e1859d1a00cbefeeee4cf62403
ivtec201 <ivtec201@gmail.com>
2019-05-11 13:11:30 +0200

    Tlumaczenie

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {            //commit zapisuje, a push wypycha na serwer; -m znaczy, że będzie wiadomość
-        System.out.println("Hello World!");
+        System.out.println("Witaj, Swiecie!");
     }
 }
