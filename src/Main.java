caedf328ab856a9b4aeeb57815d0e13a84fb227a
ivtec201 <ivtec201@gmail.com>
2019-05-11 13:03:51 +0200

    Drugi

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {            //commit zapisuje, a push wypycha na serwer; -m znaczy, że będzie wiadomość
         System.out.println("Hello World!");
     }
 }
